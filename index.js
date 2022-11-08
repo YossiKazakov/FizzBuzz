86cf54c454c5bc8d1eb44df1508532a8d3f9c021
yossikazakov <yossikazakov1@gmail.com>
2022-11-08 20:26:32 +0200

    Ex1 assignment

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-contactButton = document.querySelector("#contact button");
+let currNum = document.querySelector("#currentNumber") //The elemnt storing the current number in the bar
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+function waitAndAlert(msg) {
+  /**
+   * Since the alert is triggering BEFORE the DOM of the number is changing, this function prevents it by
+   * alerting the 'msg' string after some quantom of time
+   */
+  setTimeout(function () { alert(msg); }, 10);
+}
+
+function fizzBuzzLogic(val) {
+  /**
+   * Alerts FIZZ / BUZZ / FIZZ BUZZ according to the rules of the game
+   */
+  fizz = val % 3 === 0;
+  buzz = val % 5 === 0;
+  if (fizz && buzz) { waitAndAlert("FIZZ BUZZ"); }
+  else if (fizz) { waitAndAlert("FIZZ"); }
+  else if (buzz) { waitAndAlert("BUZZ"); }
+}
+
+function resetNumber() {
+  /**
+   * Resets the number in the bar
+   */
+  currNum.value = 0
+}
+
+function incrementNumber() {
+  /**
+   * When the user clicks on the 'increment' button, this function is called.
+   * It increments the number in the bar by one and calling the game's logic function
+   */
+  currNum.stepUp();
+  let val = currNum.valueAsNumber;
+  fizzBuzzLogic(val)
+}
